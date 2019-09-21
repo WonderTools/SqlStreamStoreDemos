1afc6b7128b25793f6eab1030831c40d40f04aa6
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-09-21 20:45:47 +0530

    InitialCommit

---
+++ SqlStreamStoreDemos/SqlStreamStoreDemos/CreatingInMemoryStreamStoreTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using SqlStreamStore;
+
+namespace SqlStreamStoreDemos
+{
+    public class CreatingInMemoryStreamStoreTest
+    {
+        [Test]
+        public void Test()
+        {
+            var streamStore = new InMemoryStreamStore();
+        }
+    }
+}
